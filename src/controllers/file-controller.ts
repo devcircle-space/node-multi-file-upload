4698724cbe3a9b1ae1170150a5a6317388594abf
Aakash Jha <aakash.jha1103@gmail.com>
2022-08-17 03:15:55 +0530

    Creating and returning an  array of uploaded files

--- src/controllers/file-controller.ts
+++ src/controllers/file-controller.ts
@@ -1,13 +1,33 @@
 import { Request, Response } from "express";
+import { IUpload } from "../interfaces";
+
+var uploadedFiles: IUpload[] = [];
 
 const FileController = {
 	uploadOne: async (req: Request, res: Response) => {
-		return res.status(200).json({ message: "1 file received", data: req.file });
+		if (!req.file) return res.status(400).json({ message: "Files not found!" });
+		const { file } = req;
+		const newFile: IUpload = {
+			fileName: file.originalname,
+			size: file.size,
+			type: file.mimetype,
+		};
+		uploadedFiles.push(newFile);
+		return res.status(200).json({ message: "1 file received", data: uploadedFiles });
 	},
+
 	uploadMany: async (req: Request, res: Response) => {
 		if (!req.files) return res.status(400).json({ message: "Files not found!" });
-		const { files } = req;
-		return res.status(200).json({ message: `${files.length} files received`, data: files });
+		const FILES = req.files as Express.Multer.File[];
+		FILES.forEach((file) => {
+			const newFile: IUpload = {
+				fileName: file.originalname,
+				size: file.size,
+				type: file.mimetype,
+			};
+			uploadedFiles.push(newFile);
+		});
+		return res.status(200).json({ message: `${FILES.length} files received`, data: uploadedFiles });
 	},
 };
 
---
+++ src/interfaces/IUpload.ts
@@ -0,0 +1,6 @@
+export default interface IUpload {
+	fileName: string;
+	size: number;
+	type: string;
+}
+
