aebd89d2d7813bb3e02913d15a85c228150c1936
Aakash Jha <aakash.jha1103@gmail.com>
2022-08-17 02:24:45 +0530

    Added config for multer

---
+++ src/utils/multer.config.ts
@@ -0,0 +1,33 @@
+import { FileFilterCallback, diskStorage } from "multer";
+import { Request } from "express";
+import { mkdirSync } from "fs";
+import { resolve } from "path";
+
+// array of allowed mimeTypes that can be uploaded
+const fileTypes = ["image/png", "image/jpg", "image/jpeg"];
+
+type DestinationCallback = (error: Error | null, destination: string) => void;
+type FileNameCallback = (error: Error | null, filename: string) => void;
+
+const MulterConfig = {
+	storage: diskStorage({
+		destination: (req: Request, file: Express.Multer.File, cb: DestinationCallback): void => {
+			const path = resolve(__dirname, "..", "temp");
+			mkdirSync(path, { recursive: true });
+			cb(null, path);
+		},
+		filename: (req: Request, file: Express.Multer.File, cb: FileNameCallback) => {
+			cb(null, `${Date.now()}-${file.originalname}`);
+		},
+	}),
+	fileFilter: (req: Request, file: Express.Multer.File, cb: FileFilterCallback): void => {
+		if (fileTypes.includes(file.mimetype)) {
+			cb(null, true);
+		} else {
+			cb(new Error("File type not allowed"));
+		}
+	},
+};
+
+export default MulterConfig;
+
